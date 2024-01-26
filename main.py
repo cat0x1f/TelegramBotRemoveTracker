ac20beba4190b0aedef6af3260b1af64378abaab
SenYue <sencat31@outlook.com>
2024-01-26 20:44:27 +0800

    修复

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import requests
 
 USER = ["@cat0x1f"]  # 只接收指定用户的消息
 TARGET_CHANNEL = "@catweb31"  # 发送到这个频道
-BOT_TOKEN = sys.argv[1]
 
+BOT_TOKEN = sys.argv[1]
 bot = telebot.TeleBot(BOT_TOKEN, parse_mode="MARKDOWN")
 
 
@@ -15,21 +15,26 @@ bot = telebot.TeleBot(BOT_TOKEN, parse_mode="MARKDOWN")
 def receive_url(message):
     try:
         message_text = message.text
-        if "https://x.com" in message_text or "https://twitter.com" in message_text:
-            bot.send_message(TARGET_CHANNEL, clean_twitter(message_text), "HTML")
-        elif "https://b23.tv" in message_text or "https://bilibili.com" in message_text:
-            print(clean_bilibili(message_text))
-            bot.send_message(TARGET_CHANNEL, clean_bilibili(message_text), "HTML")
+        bot.send_message(TARGET_CHANNEL, conditional_execution(message_text), "HTML")
 
     except:
         bot.reply_to(message, f"坏了，你发送的这条消息没法被清理。")
 
 
+def conditional_execution(message_text: str):
+    if "https://x.com" in message_text or "https://twitter.com" in message_text:
+        return clean_twitter(message_text)
+    elif "https://b23.tv" in message_text or "https://bilibili.com" in message_text:
+        return clean_bilibili(message_text)
+
+
 def clean_twitter(message_text):
-    first_question_mark_index = message_text.find("?")
-    if first_question_mark_index != -1:
-        input_str = input_str[:first_question_mark_index]
-    cleaned_link = input_str.replace("x.com", "fxtwitter.com")
+    if "?" in message_text:
+        first_question_mark_index = message_text.find("?")
+        if first_question_mark_index != -1:
+            message_text = message_text[:first_question_mark_index]
+
+    cleaned_link = message_text.replace("x.com", "fxtwitter.com")
 
     return cleaned_link
 
@@ -39,13 +44,19 @@ def clean_bilibili(message_text):
         message_text = re.findall(r"https://\S+", message_text)[-1]
         target_url = requests.get(message_text, allow_redirects=False).text
         return re.search(r'<a href="(.*?)\?', target_url).group(1)
+
     elif (
         "https://bilibili.com" in message_text
         or "https://www.bilibili.com" in message_text
     ):
-        return message_text.split("?")[0]
-    elif "?" not in message_text:
+        if "?" in message_text:
+            return message_text.split("?")[0]
+        else:
+            return message_text
+
+    else:
         return message_text
 
 
-bot.infinity_polling()
+if __name__ == "__main__":
+    bot.infinity_polling()
